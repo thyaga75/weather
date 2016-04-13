927d792efeee404e8198ac68c517fd2eaa95dcfb
THYAGA <thyaga@THYAGARAJULUs-MacBook-Pro.local>
2016-04-13 06:48:42 -0700

    first commit

---
+++ src/test/java/com/yahoo/weather/platform/Utilities.java
@@ -0,0 +1,41 @@
+package com.yahoo.weather.platform;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.jayway.jsonpath.JsonPath;
+
+public class Utilities {
+	public String readWeather(String url, String jpath) throws IOException {
+		String weather = null;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpPost httpSet = new HttpPost(url);
+			CloseableHttpResponse response1 = httpclient.execute(httpSet);
+			try {
+				System.out.println(response1.getStatusLine());
+				HttpEntity entity1 = response1.getEntity();
+				//System.out.println("entity1--->>>" + entity1);
+				String body = EntityUtils.toString(entity1);
+				//System.out.println("body--->>>" + body);
+				weather = JsonPath.parse(body).read(jpath);
+				//System.out.println("weaher--->>>" + weather);
+
+			} finally {
+				response1.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			httpclient.close();
+		}
+		return weather;
+	}
+}
---
+++ src/test/java/com/yahoo/weather/platform/WeatherTest.java
@@ -0,0 +1,24 @@
+package com.yahoo.weather.platform;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.yahoo.weather.platform.Utilities;
+
+public class WeatherTest {
+	@Test
+	public void testgetCustomerNameHttp() {
+		try {
+			Utilities ult = new Utilities();
+			String baseURL = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+			String wPath = "$.query.results.channel.item.condition.text";
+			String actualCondition = ult.readWeather(baseURL, wPath);
+			String expectedCondition = "Mostly Clear";
+			System.out.println("Expected : " + expectedCondition.toString() + "    Actual : " + actualCondition);
+			Assert.assertEquals(actualCondition, expectedCondition);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
