ab4a1a5a8c3fe0394b786679be437ac70d82550f
thyaga <thyagarajulunaidu@yahoo.com>
2016-04-13 07:37:42 -0700

    first commit

--- src/test/java/com/yahoo/weather/platform/WeatherTest.java
+++ src/test/java/com/yahoo/weather/platform/WeatherTest.java
@@ -13,7 +13,7 @@ public class WeatherTest {
 			String baseURL = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 			String wPath = "$.query.results.channel.item.condition.text";
 			String actualCondition = ult.readWeather(baseURL, wPath);
-			String expectedCondition = "Mostly Clear";
+			String expectedCondition = "Mostly Cloudy";
 			System.out.println("Expected : " + expectedCondition.toString() + "    Actual : " + actualCondition);
 			Assert.assertEquals(actualCondition, expectedCondition);
 		} catch (Exception e) {
